753d576a4ce2358825a4f4db2bf2b5adefba5fc0
linavell <linavell@naver.com>
2023-08-08 01:01:49 +0900

    feat: 'User 관련 API 개발'

---
+++ src/main/java/com/puzzle/likelion/service/UserService.java
@@ -0,0 +1,29 @@
+package com.puzzle.likelion.service;
+
+
+import com.puzzle.likelion.dto.AddUserRequest;
+import com.puzzle.likelion.entity.User;
+import com.puzzle.likelion.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto) {
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+
+    public User findById(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(()->new IllegalArgumentException("Unexpected user"));
+    }
+}
